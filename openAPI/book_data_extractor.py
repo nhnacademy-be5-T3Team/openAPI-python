f70be8f9b6dadb88b0af3dfde4ab213340511a21
yujinKim <yujinkimvv@gmail.com>
2024-03-31 09:49:10 +0900

    feat : #1 book category 테이블 데이터 insert 기능 구현

--- openAPI/book_data_extractor.py
+++ openAPI/book_data_extractor.py
@@ -1,4 +1,5 @@
 from mysql.insert_query import *
+from openAPI.category_data_extractor import save_df_to_csv
 from openAPI_request import *
 import pandas as pd
 
@@ -8,24 +9,53 @@ logger = logging.getLogger(__name__)
 
 def isbn_list_by_category_id(df):
     """주어진 카테고리 아이디로 도서 검색 OpenAPI를 요청하여 도서 ISBN 목록을 반환합니다"""
-    list = []
+    result_list = []
     for index, row in df.iterrows():
         category_name = row['2Depth']
         category_id = row['CID_x']
 
-        isbn_list = openAPI_request_search(category_name, category_id)
+        isbn_list = openAPI_request_search(category_name.split('/')[0], category_id)
+        logger.info(f"카테고리 : '{category_name.split('/')[0]}'에 대한 도서 검색 api 요청")
 
-        random_values = random.sample(isbn_list, 2)
+        if len(isbn_list) >= 2:
+            random_isbn_list = random.sample(isbn_list, 2)
+        else:
+            random_isbn_list = isbn_list
 
         if isbn_list:
-            dict = {
+            data = {
                 "category_name": category_name,
-                "category_id": category_id,
-                "isbn_list": random_values
+                "category_id": row['db_child_category_id'],
+                "isbn_list": random_isbn_list
             }
-            list.append(dict)
+            result_list.append(data)
 
-    return list
+    result_df = pd.DataFrame(result_list)
+    result_df.to_csv('output.csv', index=False)
+    save_df_to_csv(result_df, "openAPI_isbn.csv")
+
+    return result_list
+
+
+def read_csv_to_list(file_path):
+    """CSV 파일을 읽어 리스트 안의 딕셔너리 구조로 반환합니다."""
+    result_list = []
+    try:
+        df = pd.read_csv(file_path)
+
+        for index, row in df.iterrows():
+            data = {
+                "category_name": row['category_name'],
+                "category_id": row['category_id'],
+                "isbn_list": eval(row['isbn_list'])
+            }
+            result_list.append(data)
+
+        return result_list
+
+    except Exception as e:
+        print(f"CSV 파일을 읽는 중 오류가 발생했습니다: {e}")
+        return None
 
 
 if __name__ == "__main__":
@@ -33,6 +63,8 @@ if __name__ == "__main__":
     df = pd.read_csv(csv_file_path)
 
     isbn_list = isbn_list_by_category_id(df)
+
+    #isbn_list = read_csv_to_list("resources/openAPI_isbn.csv")
 
     for item in isbn_list:
         isbns = item['isbn_list']
